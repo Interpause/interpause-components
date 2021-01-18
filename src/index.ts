6d9d229dfa58169b1538b87e655569df10c753f0
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2021-01-18 14:26:15 +0800

    remove export namespaces

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export * as aesthetic from './aesthetic';
-export * as containers from './containers';
-export * as display from './display';
-export * as feedback from './feedback';
-export * as input from './input';
-export * as nav from './nav';
-export * as theme from './theme';
-export * as utils from './utils';
+export * from './aesthetic';
+export * from './containers';
+export * from './display';
+export * from './feedback';
+export * from './input';
+export * from './nav';
+export * from './theme';
+export * from './utils';
