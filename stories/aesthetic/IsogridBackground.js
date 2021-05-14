80cf88017ed5396d546a1a6e2ea6766e09d61d58
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2021-05-15 00:07:18 +0800

    cHrOmaTIc + regen typedocs again gotta hate how it reads the published readme

--- stories/aesthetic/IsogridBackground.js
+++ stories/aesthetic/IsogridBackground.js
@@ -16,14 +16,14 @@ const Template = ({ theme, ...args }) => <div style={{height:'95vh',width:'95vw'
 
 export const Normal = Template.bind({});
 Normal.args = {
-	rows:7,
-	cols:12
+	rows:5,
+	cols:7
 };
 
 export const Pretty = Template.bind({});
 Pretty.args = {
-	rows:7,
-	cols:12,
+	rows:5,
+	cols:7,
 	gapRatio:-3,
 	heightRatio:5
 };
