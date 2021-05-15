3b570f1f1aa63902afe383d9acf804a8074c3084
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2021-05-15 16:31:41 +0800

    fix stories after changing card behaviour

--- stories/theme/DarkTheme.js
+++ stories/theme/DarkTheme.js
@@ -13,22 +13,26 @@ const cards = [
 		href: 'https://nextjs.org/docs',
 		title: 'Documentation →',
 		body: 'Find in-depth information about Next.js features and API.',
+		footer: 'click here'
 	},
 	{
 		href: 'https://nextjs.org/learn',
 		title: 'Learn →',
 		body: 'Learn about Next.js in an interactive course with quizzes!',
+		footer: 'click here'
 	},
 	{
 		href: 'https://github.com/vercel/next.js/tree/master/examples',
 		title: 'Examples →',
 		body: 'Discover and deploy boilerplate example Next.js projects.',
+		footer: 'click here'
 	},
 	{
 		href:
 			'https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
 		title: 'Deploy →',
 		body: 'Instantly deploy your Next.js site to a public URL with Vercel.',
+		footer: 'click here'
 	},
 ];
 
